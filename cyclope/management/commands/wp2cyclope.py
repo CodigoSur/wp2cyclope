02109af539a5481618ec385a674517e47f09ae3a
roberto <webmaster@numerica.cl>
2015-12-12 17:50:37 -0300

    normalized & ordered output info

--- cyclope/management/commands/wp2cyclope.py
+++ cyclope/management/commands/wp2cyclope.py
@@ -6,6 +6,7 @@ import re
 from cyclope.apps.articles.models import Article
 from django.contrib.sites.models import Site
 from django.db import transaction
+from cyclope.apps.staticpages.models import StaticPage
 
 class Command(BaseCommand) :
     help = """Migrates a site in WordPress to Cyclope CMS.
@@ -53,40 +54,36 @@ class Command(BaseCommand) :
 
     def handle(self, *args, **options):
         """WordPress to Cyclope DataBase Migration Logic."""
+        print"""
+        :::::::::::wp2cyclope::::::::::::
+        ::WordPress to Cyclope migrator::
+        :::::::::::::::::::::::::::::::::\n\n-> hola, amigo!"""
+
         self.wp_prefix = options['wp_prefix']
-        #clear cyclope sqlite database
+        
+        print "-> clearing cyclope sqlite database..."
         self._clear_cyclope_db()
-        #connect to wordpress mysql database
+        
+        print "-> connecting to wordpress mysql database..."
         cnx = self._mysql_connection(options['server'], options['db'], options['user'], options['password'])
         
         # SiteSettings <- wp_options
-        # https://codex.wordpress.org/Option_Reference
-        wp_options = ('siteurl', 'blogname', 'blogdescription', 'home', 'default_comment_status', 'comment_moderation', 'comments_notify')
-        #TODO comment_registration, users_can_register, blog_public
-        wp_options = self._fetch_wp_options(cnx, wp_options)
-        settings = SiteSettings()
-        settings.global_title = wp_options['blogname']
-        settings.description = wp_options['blogdescription']
-        #NOTE settings.keywords = WP doesn't use meta tags, only as a plugin
-        settings.allow_comments = u'YES' if wp_options['default_comment_status']=='open' else u'NO'
-        settings.moderate_comments = wp_options['comment_moderation']==1 #default False
-        settings.enable_comments_notifications = wp_options['comments_notify'] in ('', 1) #default True
-        site = Site()
-        site.name = wp_options['blogname']
-        site.domain = wp_options['siteurl'] 
-        site.save()
-        settings.site = site
-        settings.save()
+        settings = self._fetch_site_settings(cnx)
+        print "-> nice to meet you, "+settings.site.name
         
         # Article <- wp_posts
-        wp_post_fields = ('post_title', 'post_status', 'post_date', 'post_modified', 'comment_status', 'post_content', 'post_excerpt')
-        wp_posts_count = self._fetch_wp_posts(cnx, wp_post_fields)
-        
-        # StaticPage <- wp_posts
-        # TODO <-- ! 
+        wp_posts_a_count, articles_count = self._fetch_articles(cnx)
+        print "-> migrated {} articles out of {} posts".format(articles_count, wp_posts_a_count)
 
+        # StaticPage <- wp_posts
+        wp_posts_p_count, pages_count = self._fetch_pages(cnx)
+        print "-> migrated {} static pages out of {} posts".format(pages_count, wp_posts_p_count)    
+
+        #...
         #close mysql connection
         cnx.close()
+        # WELCOME
+    ####
 
     def _mysql_connection(self, host, database, user, password):
         """Establish a MySQL connection to the given option params and return it."""
@@ -110,19 +107,43 @@ class Command(BaseCommand) :
         else:
             return cnx
 
-    def _fetch_wp_options(self, mysql_cnx, options):
+    def _clear_cyclope_db(self):
+        SiteSettings.objects.all().delete()
+        Site.objects.all().delete()
+        Article.objects.all().delete()
+        StaticPage.objects.all().delete()
+
+    def _fetch_site_settings(self, mysql_cnx):
         """Execute single query to WP _options table to retrieve the given option names."""
+        options = ('siteurl', 'blogname', 'blogdescription', 'home', 'default_comment_status', 'comment_moderation', 'comments_notify')
+        #TODO comment_registration, users_can_register, blog_public
+        #single query
         query = "SELECT option_name, option_value FROM "+self.wp_prefix+"options WHERE option_name IN {}".format(options)
         cursor = mysql_cnx.cursor()
         cursor.execute(query)
-        results = dict(cursor.fetchall())
+        wp_options = dict(cursor.fetchall())
         cursor.close()
-        return results
+        #
+        settings = SiteSettings()
+        settings.global_title = wp_options['blogname']
+        settings.description = wp_options['blogdescription']
+        #NOTE settings.keywords = WP doesn't use meta tags, only as a plugin
+        settings.allow_comments = u'YES' if wp_options['default_comment_status']=='open' else u'NO'
+        settings.moderate_comments = wp_options['comment_moderation']==1 #default False
+        settings.enable_comments_notifications = wp_options['comments_notify'] in ('', 1) #default True
+        site = Site()
+        site.name = wp_options['blogname']
+        site.domain = wp_options['siteurl'] 
+        site.save()
+        settings.site = site
+        settings.save()
+        return settings
 
-    def _fetch_wp_posts(self, mysql_cnx, fields):
-        """Queries the given fields to WP posts table selecting only posts, not pages nor attachments,
+    def _fetch_articles(self, mysql_cnx):
+        """Queries the given fields to WP posts table selecting only posts, not pages nor attachments nor revisions,
            It parses data as key-value pairs to instance rows as Articles and save them.
            Returns the number of created Articles and of fetched rows in a tuple."""
+        fields = ('post_title', 'post_status', 'post_date', 'post_modified', 'comment_status', 'post_content', 'post_excerpt')
         query = re.sub("[()']", '', "SELECT {} FROM ".format(fields))+self.wp_prefix+"posts WHERE post_type='post'"
         cursor = mysql_cnx.cursor()
         cursor.execute(query)
@@ -130,15 +151,35 @@ class Command(BaseCommand) :
         transaction.enter_transaction_management()
         transaction.managed(True)
         for wp_post in cursor :
-            article = self._post_2_article(dict(zip(fields, wp_post)))
+            article = self._post_to_article(dict(zip(fields, wp_post)))
             article.save() 
         transaction.commit()
         transaction.leave_transaction_management()
-        counts = (Article.objects.count(), cursor.rowcount)
+        counts = (cursor.rowcount, Article.objects.count())
         cursor.close()
         return counts 
 
-    def _post_2_article(self, post):
+    def _fetch_pages(self, mysql_cnx):
+        """Queries to WP posts table selecting only pages, not posts nor attachments nor revisions."""
+        fields = ('post_title','post_status','post_date', 'post_modified',  'comment_status', 'post_content', 'post_excerpt')
+        query = re.sub("[()']", '', "SELECT {} FROM ".format(fields))+self.wp_prefix+"posts WHERE post_type='page'"
+        cursor = mysql_cnx.cursor()
+        cursor.execute(query)
+        #single transaction for all pages
+        transaction.enter_transaction_management()
+        transaction.managed(True)
+        for wp_post in cursor :
+            page = self._post_to_static_page(dict(zip(fields, wp_post)))
+            page.save()
+        transaction.commit()
+        transaction.leave_transaction_management()
+        counts = (cursor.rowcount, StaticPage.objects.count())
+        cursor.close()
+        return counts 
+
+
+    #TODO 15+ Failed to populate slug Article.slug from name
+    def _post_to_article(self, post):
         """Instances an Article object from a WP post hash."""
         return Article(
             name = post['post_title'],
@@ -148,22 +189,24 @@ class Command(BaseCommand) :
             creation_date = post['post_date'],
             modification_date = post['post_modified'],
             published = post['post_status']=='publish',#private and draft are unpublished
-            allow_comments = post['comment_status']=='open',#all posts have a status, none imported default to SITE
+            #TODO all posts have a status, they are saved as the option that's set(?).
+            #if the user then tries to close them all, he shouldn't set them one by one.
+            #whe should set them to SITE default unless comments are explicitly closed, which is the minority(?)       
+            allow_comments = post['comment_status']=='open',
             summary = post['post_excerpt']
             #pretitle has no equivalent in WP
-            #TODO 
-            #show_author=always user
-            #FKs:
-            #   user
-            #   comments
-            #   related_contents
-            #   picture
-            #   author
-            #   source
+            #TODO show_author=always user #FKs: user comments related_contents picture author source
         )
 
-    def _clear_cyclope_db(self):
-        SiteSettings.objects.all().delete()
-        Site.objects.all().delete()
-        Article.objects.all().delete()
+    def _post_to_static_page(self, post):
+        return StaticPage(
+            name = post['post_title'],
+            text = post['post_content'],
+            creation_date = post['post_date'],
+            modification_date = post['post_modified'],
+            published = post['post_status']=='publish',#private and draft are unpublished
+            allow_comments = post['comment_status']=='open',#TODO see article's allow_comments
+            summary = post['post_excerpt']
+            #TODO user related_contents comments show_author
+        )
 
